8f024bec79d71aaa1461b255fde520ee17bd8a0b
Caio Neves <caio.nevesleite@gmail.com>
2018-01-25 06:33:59 +0000

    Text darker

--- db_commands.sql
+++ db_commands.sql
@@ -1,25 +1,25 @@
 -- drop table users;
 -- drop table months;
 -- drop table debts;
--- create table users (id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
---                     username varchar(50) NOT NULL,
---                     password varchar(50) NOT NULL,
---                     UNIQUE (username)
---                     );
+create table users (id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                    username varchar(50) NOT NULL,
+                    password varchar(50) NOT NULL,
+                    UNIQUE (username)
+                    );
                     
--- create table months (id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
---                     month_date DATE NOT NULL,
---                     user_id int NOT NULL,
---                     UNIQUE (month_date, user_id)
---                     );
+create table months (id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                    month_date DATE NOT NULL,
+                    user_id int NOT NULL,
+                    UNIQUE (month_date, user_id)
+                    );
                     
--- create table debts (id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
---                     description varchar(50) NOT NULL,
---                     due_date DATE NOT NULL,
---                     price FLOAT NOT NULL,
---                     paid int NOT NULL,
---                     month_id int NOT NULL,
---                     UNIQUE (description, month_id)
---                     );
+create table debts (id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                    description varchar(50) NOT NULL,
+                    due_date DATE NOT NULL,
+                    price FLOAT NOT NULL,
+                    paid int NOT NULL,
+                    month_id int NOT NULL,
+                    UNIQUE (description, month_id)
+                    );
                     
-select * from  debts;
+-- select * from  debts;
